35de358b1ceda2b3ef410c04b059325869f40f75
Pierre TREHET <128218103+PierreTrehet@users.noreply.github.com>
2025-06-08 21:56:06 +0200

    Create eslint.config.js

---
+++ eslint.config.js
@@ -0,0 +1,24 @@
+import js from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+
+export default [
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    plugins: {
+      react: react,
+      'react-hooks': reactHooks
+    },
+    rules: {
+      'react/react-in-jsx-scope': 'off',
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+  },
+];
+export const ignores = ['node_modules', 'dist'];
