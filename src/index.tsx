1f998d9da8f4be8cb16e57bcf248966df706c22e
Pierre TREHET <128218103+PierreTrehet@users.noreply.github.com>
2025-06-09 11:20:40 +0200

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import Example from "./example"
+import Example from "./example/main"
 
 createRoot(document.getElementById('root')).render(<React.StrictMode><Example/></React.StrictMode>);
