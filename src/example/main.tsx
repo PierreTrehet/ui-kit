21289eb44228c644a052a8366aca275b034dc227
Pierre TREHET <128218103+PierreTrehet@users.noreply.github.com>
2025-06-09 11:55:51 +0200

    Update main.tsx

--- src/example/main.tsx
+++ src/example/main.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Example from "./example";
 
-ReactDOM.createRoot(document.getElementById("app")!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Example />
   </React.StrictMode>
