a4fe6c33581227041516a6752b8666bb9fd2614c
Pierre TREHET <128218103+PierreTrehet@users.noreply.github.com>
2025-06-09 11:56:27 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -17,18 +17,21 @@ export default defineConfig(({ command, mode }) => {
     }
   }
 
-  <!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Reef UI Showcase</title>
-  </head>
-  <body>
-    <div id="root"></div>
-    <script type="module" src="/src/index.tsx"></script>
-  </body>
-</html>
+
+ if (mode === "showcase") {
+  console.warn("This is showcase");
+  return {
+    base: "/ui-kit/",
+    plugins: [react()],
+    build: {
+      rollupOptions: {
+        input: path.resolve(__dirname, "index.html") // <-- IMPORTANT
+      }
+    }
+  };
+}
+
+
 
 
   return {
